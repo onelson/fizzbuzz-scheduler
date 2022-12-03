55c18ab87ade48beb14bfaf38e147c212aaae44f
Owen Nelson <onelson@gmail.com>
2022-12-02 23:08:49 -0800

    feat(core): stub out core API, define shared types
    The core crate is intended to provide types that will be useful in both
    processes: the http server and the worker.
    
    This is also a convenient place to try and encapsulate the storage
    concerns (i.e, database access). Ideally we'd be able to hide much of
    the database dependency types/functions but in practice this is often
    tricky or awkward. TBD if this can be achieved here.
    
    Currently, I'm yet to actually pick a client library so the actual SQL
    details are left unimplemented. The stubs should however be enough to
    build out the downstream systems, with an allowance for some small
    refactors as needed to tie it all together.

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,1 +1,69 @@
 //! Common code shared by the worker and http service.
+
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
+
+pub type ID = i32;
+pub type Timestamp = DateTime<Utc>;
+
+#[derive(Debug, Serialize)]
+pub struct Task {
+    pub id: ID,
+    pub kind: TaskType,
+    pub execution_time: Timestamp,
+    pub state: TaskState,
+    pub created: Timestamp,
+    pub updated: Timestamp,
+}
+
+impl Task {
+    pub fn run(&self) {
+        let sleep_secs = match self.kind {
+            TaskType::Fizz => 3,
+            TaskType::Buzz => 5,
+            TaskType::FizzBuzz => 15,
+        };
+
+        std::thread::sleep(std::time::Duration::from_secs(sleep_secs));
+        println!("{} {}", self.kind, self.id);
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub enum TaskType {
+    Fizz,
+    Buzz,
+    FizzBuzz,
+}
+
+impl Display for TaskType {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                TaskType::Fizz => "Fizz",
+                TaskType::Buzz => "Buzz",
+                TaskType::FizzBuzz => "Fizz Buzz",
+            }
+        )
+    }
+}
+
+// N.b. we don't really have any fallible tasks, but if we did we could
+// represent that as a state.
+#[derive(Debug, Deserialize, Serialize)]
+pub enum TaskState {
+    Pending,
+    Completed,
+}
+
+#[derive(Debug, Default, Deserialize)]
+pub struct Filters {
+    pub state: Option<TaskState>,
+    #[serde(rename = "type")]
+    pub kind: Option<TaskType>,
+}
+
+pub mod storage;
