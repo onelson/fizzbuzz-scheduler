a833b4cc1c1bc8ae8250e32bfc4938109ded41af
Owen Nelson <onelson@gmail.com>
2022-12-02 23:35:18 -0800

    feat(server): define http server and route handlers
    Essentially this rev aims to capture the API contracts described in the
    brief, even though the storage implementation isn't here yet.
    
    Per the brief, we have handlers that offer CRUD basics:
    
    - lists of tasks given filter criteria
    - Create a new task
    - Read a specific task
    - Delete a specific task
    
    Rough (not production-ready) database connection handling is here, and
    so is some supporting code for converting (`anyhow`) Error types into HTTP
    responses.

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,5 +1,77 @@
 //! HTTP service for creating tasks and checking their status.
 
-fn main() {
-    todo!("server");
+use anyhow::Context;
+use axum::{
+    extract::{Path, Query},
+    http::StatusCode,
+    response::IntoResponse,
+    routing::get,
+    Json, Router,
+};
+use errors::HandlerResult;
+use serde::Deserialize;
+use serde_json::json;
+use std::net::SocketAddr;
+use tracing::{event, Level};
+
+mod errors;
+
+// FIXME: naive to blindly reach for a fresh connection every time.
+//   Better would be to use a connection pooler and supply it to handlers
+//   via shared state.
+fn get_db() -> anyhow::Result<core::storage::Connection> {
+    // FIXME: better to expose config via CLI (with optional env vars).
+    let dsn =
+        std::env::var("DATABASE_URL").with_context(|| "DATABASE_URL is required but unset")?;
+    core::storage::Connection::new(&dsn).with_context(|| "failed to connect to database")
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/tasks", get(list_tasks).post(create_task))
+        .route("/tasks/{}", get(read_task).delete(destroy_task));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    event!(Level::DEBUG, "listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+#[derive(Deserialize)]
+struct CreateTaskRequest {
+    #[serde(rename = "type")]
+    kind: core::TaskType,
+    execution_time: core::Timestamp,
+}
+
+async fn create_task(Json(payload): Json<CreateTaskRequest>) -> HandlerResult {
+    let conn = get_db()?;
+    let task_id = core::storage::create(&conn, payload.kind, payload.execution_time)?;
+    Ok((StatusCode::CREATED, Json(json!({ "id": task_id }))).into_response())
+}
+
+async fn read_task(Path(task_id): Path<core::ID>) -> HandlerResult {
+    let conn = get_db()?;
+    // TODO: this handler should 404 if the task is None
+    let task = core::storage::read(&conn, task_id)?;
+    Ok((StatusCode::OK, Json(task)).into_response())
+}
+
+async fn destroy_task(Path(task_id): Path<core::ID>) -> HandlerResult {
+    let conn = get_db()?;
+    // TODO: deletions should be idempotent in so much as deleting an already
+    //   non-existent task should give a 204, same as a regular deletion.
+    core::storage::destroy(&conn, task_id)?;
+    Ok((StatusCode::NO_CONTENT, ()).into_response())
+}
+
+async fn list_tasks(Query(filters): Query<core::Filters>) -> HandlerResult {
+    let conn = get_db()?;
+    let tasks = core::storage::list(&conn, filters)?;
+    Ok((StatusCode::OK, Json(json!({ "results": tasks }))).into_response())
 }
